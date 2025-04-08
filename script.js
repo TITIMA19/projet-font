191618715b0621d696a7e981fc6803130738987a
fatima <assouabfatima19@gmail.com>
2025-04-08 12:38:08 +0100

    first js

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
-
-document.getElementById('calculateButton').addEventListener('click', function(event) {
-    
+ document.getElementById('calculateButton').addEventListener('click', function() {
     const mortgageAmount = document.getElementById('mortgageAmount').value;
     const mortgageTerm = document.getElementById('mortgageTerm').value;
-    const interestRate = document.getElementById('interestRate').value;
-    const errorMessage = document.getElementById('errorMessage');
-
-    errorMessage.textContent = ''; 
-
-    if (mortgageAmount.trim() === '' || mortgageTerm.trim() === '' || interestRate.trim() === '') {
-        return( errorMessage.textContent = 'Error: All fields are required.') ;
-       
-    }
-
-    console.log('Inputs:', mortgageAmount, mortgageTerm, interestRate);
+    const interestRate= document.getElementById('interestRate').value; // Get value on click
+    const amount = parseFloat(mortgageAmount,interestRate,mortgageTerm);
+    if (isNaN(amount)) {
+        alert("Please enter a valid number");
+     }else{
+        
+     }
+  
 });
 
